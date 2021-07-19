d26426593eb41e270c6eee55224e6c3dac220e0a
dnyana <dnyaneshgawade903@gmail.com>
2021-07-19 10:03:08 +0530

    [Dnyanesh] add: day 1 assignment

---
+++ Demo/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Demo
+{
+   public class Class1
+    {
+        public void Add(int a, int b)
+        {
+            int c = a + b;
+            Console.WriteLine("Addition of two number is:" + c);
+        }
+
+    }
+}
---
+++ Demo/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Class1 cl = new Class1();
+            cl.Add(1, 2);
+        }
+    }
+}
